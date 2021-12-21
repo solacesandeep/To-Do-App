36100e389357159b087dbec258e4d2b07357b8ea
solacesandeep <92437341+solacesandeep@users.noreply.github.com>
2021-12-21 12:48:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var express=require("express");
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var app=express();
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
+const itemSchema={
+    name:String
+}
+const Item=mongoose.model("Item",itemSchema);
+const item1=new Item({
+    name:"Welcome to Solace",
+});
+const item2=new Item({
+    name:"Turn off Light  ",
+});
+const item3=new Item({
+    name:"turn on light",
+});
+const d=[item1,item2,item3];
+
+app.get("/",function(req,res)
+{
+   // res.send("<h1>Hey guys!!</h1>");
+   Item.find({},function(err,f)
+   {
+      // console.log(f);
+      if(f.length===0)
+      {
+        Item.insertMany(d,function(err)
+        {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Successfully saved items to DB");
+            }
+        });
+      res.redirect("/");
+      }
+      else{
+      res.render("list",{newListItems:f});
+      }
+   })
+  ;
+})
+app.post("/",function(req,res)
+{
+     const itemName=req.body.n;
+   
+   const item=new Item({
+       name:itemName
+   });
+item.save();
+res.redirect("/");
+});
+app.post("/delete",function(req,res)
+{
+  const check=req.body.checkbox;
+  Item.findByIdAndRemove(check,function(err)
+  {
+      if(!err)
+      {
+          console.log("Successfully deleted");
+          res.redirect("/");
+      }
+  })
+});
+
+app.listen(3000,function()
+{
+    console.log("Server is listening to port 3000");
+})
+
+
+
